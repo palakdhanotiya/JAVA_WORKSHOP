e733dd01fad3fc0780a8f8b877e97d967af707b1
Palak Dhanotiya <YOUR_GITHUB_EMAIL>
2026-07-06 11:36:28 +0530

    Commit and push

---
+++ LoopControls/src/NestedLoopEx1.java
@@ -0,0 +1,25 @@
+/*public class practice {
+    public static void main(String[] args){
+
+        for(int i=1; i<=5; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+        System.out.println();
+        }
+    }
+    
+}*/
+
+public class NestedLoopEx1{
+    public static void main(String[] args){
+
+        for(int i=1; i<=5; i++){
+            for(int j=i; j<=5; j++){
+                System.out.print("* ");
+            }
+        System.out.println();
+        }
+    }
+    
+}
